9c9c2f4c5dc56fbe58908ec3aaf1d2f35a922639
Ashikur Rahman <ashik.cse.developer@gmail.com>
2024-11-22 17:07:57 +0600

    package updated

--- Package.swift
+++ Package.swift
@@ -9,7 +9,8 @@ let package = Package(
         .library(name: "Compound", targets: ["Compound"])
     ],
     dependencies: [
-        .package(url: "https://github.com/element-hq/compound-design-tokens", exact: "2.1.0"),
+//        .package(url: "https://github.com/element-hq/compound-design-tokens", exact: "2.1.0"),
+        .package(url: "https://github.com/Ashik55/encipher-compound-design-tokens.git", branch: "main"),
         .package(url: "https://github.com/siteline/SwiftUI-Introspect", from: "1.3.0"),
         .package(url: "https://github.com/SFSafeSymbols/SFSafeSymbols", from: "5.3.0"),
         .package(url: "https://github.com/BarredEwe/Prefire", from: "2.9.0"),
@@ -19,7 +20,7 @@ let package = Package(
         .target(
             name: "Compound",
             dependencies: [
-                .product(name: "CompoundDesignTokens", package: "compound-design-tokens"),
+                .product(name: "EncipherCompoundDesignTokens", package:  "encipher-compound-design-tokens"),
                 .product(name: "SwiftUIIntrospect", package: "SwiftUI-Introspect"),
                 .product(name: "SFSafeSymbols", package: "SFSafeSymbols"),
                 .product(name: "Prefire", package: "Prefire")
